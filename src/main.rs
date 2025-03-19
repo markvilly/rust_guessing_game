67752378f23096ced97f604fbf149cc127af3a71
Mark Paul <markpaulbusiness66@gmail.com>
2025-03-19 10:45:52 +0300

    Added standard io library to accept input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
- // Requirements:
- // Difficulty levels
- // Random Number
- // Time boundaries
+// Requirements:
+// Difficulty levels
+// Random Number
+// Time boundaries
 
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+
+    let high_score: Option<u32> = None;
+}
+
+fn choose_difficulty() -> u32 {
+    loop {
+        println!("Choose a difficulty level: ");
+        println!("1. Easy (1-50)");
+        println!("2. Medium (1-100");
+        println!("3. Hard (1-200");
+
+        let mut difficulty: String = String::new();
+
+        io::stdin()
+            .read_line(&mut difficulty)
+            .expect("Failed to read line");
+
+        match difficulty.trim().parse() {
+            Ok(1) => return 50,
+            Ok(2) => return 100,
+            Ok(3) => return 200,
+            _ => println!("Invalid input, please enter a valid difficulty lever (1, 2, 3)"),
+        };
+    }
 }
